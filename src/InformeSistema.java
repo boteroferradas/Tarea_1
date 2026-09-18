9e77c29ff7152dcb104969d6033c0f2ec0e83c0d
Breixo <boteroferradas@danielcastelao.org>
2026-09-18 12:29:09 +0200

    Ejercicios sobre memoria y parte de los ejercicios del sistema hechos

--- src/InformeSistema.java
+++ src/InformeSistema.java
@@ -1,4 +1,6 @@
+import java.lang.System;
 import java.lang.Runtime;
+import java.util.Scanner;
 
 public class InformeSistema {
 
@@ -19,6 +21,25 @@ public class InformeSistema {
     }
 
     public static long memoria_maxima() {
-        return Runtime.getRuntime().totalMemory();
+        return Runtime.getRuntime().maxMemory();
     }
+
+    public static String uso_porcentaje(){
+        long uso = memoria_uso();
+        long total = memoria_reservada();
+
+        if (total == 0) return "0%";
+
+        double porcentaje = ((double) uso / total) * 100;
+        return String.format("%.2f%%", porcentaje);
+    }
+
+    public static long convertirBaMB(long bytes){
+        return bytes / (1024L * 1024L);
+    }
+
+    public static String propiedadesSistema() {
+        Scanner teclado = new Scanner(System.in);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,44 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println(InformeSistema.n_procesadores);
-        System.out.println(InformeSistema.memoria_libre());
-        System.out.println(InformeSistema.memoria_reservada());
-        System.out.println(InformeSistema.memoria_uso());
-        System.out.println(InformeSistema.memoria_maxima());
+        System.out.println("PROCESADORES");
+        System.out.println("==================================================");
+        System.out.println("    Disponibles JVM:" + InformeSistema.n_procesadores());
+
+        System.out.println("MEMORIA - ANTES");
+        System.out.println("==================================================");
+        long usoantes = InformeSistema.memoria_uso();
+        System.out.println("    Total reservada:        " + InformeSistema.convertirBaMB(InformeSistema.memoria_reservada()) + " MiB");
+        System.out.println("    Libre:      " + InformeSistema.convertirBaMB(InformeSistema.memoria_libre()) + " MiB");
+        System.out.printf("    En uso:      " + InformeSistema.convertirBaMB(usoantes) + " MiB");
+        System.out.println(" (" + InformeSistema.uso_porcentaje() + " de la total)");
+        System.out.println("    Maxima:     " + InformeSistema.convertirBaMB(InformeSistema.memoria_maxima()) + " MiB");
+        long[] reservado = new long[8 * 1024 * 1024];
+
+        System.out.println("MEMORIA - DESPUES DE RESERVAR 64 MIB");
+        System.out.println("==================================================");
+        long usodepues = InformeSistema.memoria_uso();
+        System.out.println("    Total reservada:        " + InformeSistema.convertirBaMB(InformeSistema.memoria_reservada()) + " MiB");
+        System.out.println("    Libre:      " + InformeSistema.convertirBaMB(InformeSistema.memoria_libre()) + " MiB");
+        System.out.printf("    En uso:      " + InformeSistema.convertirBaMB(InformeSistema.memoria_uso()) + " MiB");
+        System.out.println(" (" + InformeSistema.uso_porcentaje() + " de la total)");
+        System.out.println("    Maxima:     " + InformeSistema.convertirBaMB(InformeSistema.memoria_maxima()) + " MiB");
+        long incrementoMemoria = usodepues - usoantes;
+        System.out.println("    Incremento en uso: " + InformeSistema.convertirBaMB(incrementoMemoria) + "MiB");
+        System.out.println("El array sigue en memoria: reservado[0] = " + reservado[0]);
+
+        System.out.println("SISTEMA");
+        System.out.println("==================================================");
+        String sistema = System.getProperty("os.name");
+        String separador = System.getProperty("file.separator");
+        String userHome = System.getProperty("user.home");
+        System.out.println("os.name: " + sistema);
+        System.out.println("file.separator: '" + separador+ "'");
+        System.out.println("Ruta construida con las propiedades: ");
+        String ruta = userHome + separador + "psp" + separador + "informe.txt";
+        System.out.println(ruta);
+
+        System.out.println("PROPIEDADES QUE EMPIEZAN POR os., user., java.version");
+        System.out.println("==================================================");
+
     }
 }
